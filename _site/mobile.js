4acb1dd70be7b2c18be919b36c1eb7df4039284a
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-01-29 16:46:38 +1030

    stop marquee for mobile

--- _site/mobile.js
+++ _site/mobile.js
@@ -3,13 +3,17 @@
     // document.getElementByClassName('marquee').innerHTML = "SORRYYYYYYYYY";
 
     var pet = document.getElementById("gifypet");
+    var marquee = document.getElementById("marquee");
+    var runner = document.getElementById("runner");
 
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        console.log("mobile");
+        // console.log("mobile");
         pet.style.display = "none";
+        marquee.style.textAlign = "center";
+        runner.style.animation = "none";
     } else {
-        console.log("not mobile");
+        //console.log("not mobile");
         pet.style.display = "block";
     }
 
