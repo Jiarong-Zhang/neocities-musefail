20d56e97fbe826728a724b72a530cadeecb553ce
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-01-29 16:02:02 +1030

    maybe this will work

--- _site/mobile.js
+++ _site/mobile.js
@@ -1,8 +1,16 @@
 (function() {
 
-    //var elem = document.getElementByClassName("marquee");
-    //elem.style.display = none;
+    // document.getElementByClassName('marquee').innerHTML = "SORRYYYYYYYYY";
 
-    document.getElementByClassName('marquee').innerHTML = "SORRYYYYYYYYY";
+    var pet = document.getElementById("gifypet");
+
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        console.log("mobile");
+        pet.style.display = "none";
+    } else {
+        console.log("not mobile");
+        pet.style.display = "block";
+    }
 
 }());
