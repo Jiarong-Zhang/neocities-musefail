1cca62630a2c528c61c03c1886ef32047e27f5ba
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-01-29 14:26:20 +1030

    prettify

--- _site/gallery.js
+++ _site/gallery.js
@@ -10,32 +10,32 @@
 // Don't edit below this line unless you know what you're doing!
 ///
 
-(function () {
+(function() {
 
-const defaultOptions = {
-  draggable: false,
-  resizable: false,
-  initMaximized: true,
-  headerToolbar: ['close'],
-  footerToolbar: ['prev', 'zoomOut', 'actualSize', 'zoomIn', 'next'],
-  title: false,
-  multiInstances: true,
-};
+    const defaultOptions = {
+        draggable: false,
+        resizable: false,
+        initMaximized: true,
+        headerToolbar: ['close'],
+        footerToolbar: ['prev', 'zoomOut', 'actualSize', 'zoomIn', 'next'],
+        title: false,
+        multiInstances: true,
+    };
 
-const galleries = document.getElementsByClassName("ezgallery");
+    const galleries = document.getElementsByClassName("ezgallery");
 
-for (let i = 0; i < galleries.length; i++) {
-  let links = [...galleries[i].getElementsByTagName("a")];
+    for (let i = 0; i < galleries.length; i++) {
+        let links = [...galleries[i].getElementsByTagName("a")];
 
-  for (let j = 0; j < links.length; j++) {
-    let items = [];
-    let itemOptions = {...defaultOptions, index: j};
-    links.map((link) => items.push({src: link.href}));
-    links[j].addEventListener('click', (event) => {
-      event.preventDefault();
-      new PhotoViewer(items, itemOptions);
-    }, false);
-  }
-}
+        for (let j = 0; j < links.length; j++) {
+            let items = [];
+            let itemOptions = { ...defaultOptions, index: j };
+            links.map((link) => items.push({ src: link.href }));
+            links[j].addEventListener('click', (event) => {
+                event.preventDefault();
+                new PhotoViewer(items, itemOptions);
+            }, false);
+        }
+    }
 
 }());
