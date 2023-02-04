f665858582513ba3888a19cb05cbddf34f4fc983
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-02-04 22:02:11 +1030

    less eye movemnt

--- _site/mobile.js
+++ _site/mobile.js
@@ -6,6 +6,7 @@
     var marquee = document.getElementById("marquee");
     //var marquee_content = document.getElementById("marquee_content");
     var footer = document.getElementById("footer");
+    var blog = document.getElementByClass("blog");
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // console.log("mobile");
@@ -14,6 +15,7 @@
         //marquee.style.textAlign = "center";
         //marquee_content.style.animation = "none";
         footer.style.fontSize = "75%";
+        blog.style.width = "95%";
     } else {
         //console.log("not mobile");
         pet.style.display = "block";
