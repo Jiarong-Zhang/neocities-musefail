d0e621d9e59c69e3845798ecef37f31848b2f1a7
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-02-04 20:47:55 +1030

    footer

--- _site/mobile.js
+++ _site/mobile.js
@@ -4,14 +4,16 @@
 
     var pet = document.getElementById("gifypet");
     var marquee = document.getElementById("marquee");
-    var marquee_content = document.getElementById("marquee_content");
-
+    //var marquee_content = document.getElementById("marquee_content");
+    var footer = document.getElementById("footer");
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // console.log("mobile");
         pet.style.display = "none";
-        marquee.style.textAlign = "center";
-        marquee_content.style.animation = "none";
+        marquee.style.display = "none";
+        //marquee.style.textAlign = "center";
+        //marquee_content.style.animation = "none";
+        footer.style.transform = "scale(0.75)";
     } else {
         //console.log("not mobile");
         pet.style.display = "block";
