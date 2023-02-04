55fd4c29a6644f59ca062870c8dcbc1bb5c9bb7e
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-02-04 22:37:25 +1030

    Update mobile.js

--- _site/mobile.js
+++ _site/mobile.js
@@ -2,23 +2,31 @@
 
     // document.getElementByClassName('marquee').innerHTML = "SORRYYYYYYYYY";
 
-    var pet = document.getElementById("gifypet");
-    var marquee = document.getElementById("marquee");
+    //var pet = document.getElementById("gifypet");
+    //var marquee = document.getElementById("marquee");
     //var marquee_content = document.getElementById("marquee_content");
     var footer = document.getElementById("footer");
-    var blog = document.querySelector(".div_center.blog");
+    //var blog = document.querySelector(".div_center.blog");
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // console.log("mobile");
-        pet.style.display = "none";
-        marquee.style.display = "none";
+
+        if (window.location.href == "https://musefail.neocities.org") {
+
+            document.getElementById("gifypet").style.display = "none";
+            document.getElementById("marquee").style.display = "none";
+        } else {
+
+            document.querySelector(".div_center.blog").style.width = "95%";
+
+        }
+        footer.style.fontSize = "50%";
         //marquee.style.textAlign = "center";
         //marquee_content.style.animation = "none";
-        //footer.style.fontSize = "50%";
-        blog.style.width = "95%";
+
     } else {
         //console.log("not mobile");
-        pet.style.display = "block";
+        //pet.style.display = "block";
     }
 
 }());
