1b23d3bcbc5aff7e3e4aec3fc78dc8ee5a1b562e
emily zhang <64816058+Jiarong-Zhang@users.noreply.github.com>
2023-02-04 22:57:31 +1030

    update htmls

--- _site/mobile.js
+++ _site/mobile.js
@@ -8,18 +8,22 @@
     var footer = document.getElementById("footer");
     //var blog = document.querySelector(".div_center.blog");
 
+    var url = window.location.href;
+    const blog_url_pattern = /https\:\/\/musefail.neocities.org\/blogs\/entry_/;
+
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // console.log("mobile");
 
-        if (window.location.href == "https://musefail.neocities.org") {
+        if (url == "https://musefail.neocities.org/") {
 
             document.getElementById("gifypet").style.display = "none";
             document.getElementById("marquee").style.display = "none";
-        } else {
+
+        } else if (blog_url_pattern.test(url) == true) {
 
             document.querySelector(".div_center.blog").style.width = "95%";
-
         }
+
         footer.style.fontSize = "50%";
         //marquee.style.textAlign = "center";
         //marquee_content.style.animation = "none";
